85aa20840a264d9cbcfd1743252650a1c11b1f4b
Beniamin <106469071+redve-dev@users.noreply.github.com>
2023-01-04 22:28:34 +0100

    refactor

--- lua/my_plugin/init.lua
+++ lua/my_plugin/init.lua
@@ -4,10 +4,13 @@ local function is_git()
 end
 
 local function count_diff()
+	if not is_git() then
+		return ""
+	end
 	local diff = vim.fn.system({"git", "diff", "--numstat"})
 	--https://stackoverflow.com/questions/1426954/split-string-in-lua
 	-- fr, lua has tons of string methods, but not a split??
-	local function mysplit (inputstr, sep)
+	local function split_string(inputstr, sep)
 		if sep == nil then
 			sep = "%s"
 		end
@@ -17,11 +20,10 @@ local function count_diff()
 		end
 		return t
 	end
-	local lines = mysplit(diff, "	")
+	local lines = split_string(diff, "	")
 	local added = tonumber(lines[1])
 	local removed = tonumber(lines[2])
-	print("added: "..added)
-	print("removed: "..removed)
+	return {added= added, removed=removed}
 end
 
 return {
